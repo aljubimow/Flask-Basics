a1d52ac2c593e67313ee562fe80582da4bd9efd0
Alex Ljubimow <ljubimow@juno.com>
2018-03-25 16:57:06 -0700

    first commit

---
+++ assignment_counter/counter.py
@@ -0,0 +1,26 @@
+# Counter of Visitors
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = 'ThisIsSecret'
+
+@app.route('/')
+def index():
+    if "count" not in session:
+        session["count"] = 0
+        #set session counter to zero
+    session["count"] +=1
+    #adds 1 when refreshed
+    return render_template('counter.html')
+    
+@app.route('/increment', methods=['POST'])
+def increment_by_two():
+    session['count'] += 1
+    #adds 2 when button is clicked
+    return redirect('/')
+
+@app.route('/clear', methods=['POST'])
+def clear():
+    session['count'] = 0
+    return redirect('/')
+
+app.run(debug=True)
